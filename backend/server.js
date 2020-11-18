c99c7b0fd48e3e117083c24d21780d0c6b61b856
liamlows <liam.lowsleywilliams@gmail.com>
2020-11-18 17:05:35 -0600

    changed to pools for mysql, moved routes to seperate file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mysql = require('mysql');
+const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
-
-// mysql connection
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_CLOUD_HOST,
-  user: process.env.MYSQL_CLOUD_USER,
-  password: process.env.MYSQL_CLOUD_PASS,
-  port: process.env.MYSQL_PORT,
-  database: process.env.MYSQL_DB
-});
+// const mysqlConnect = require('./db');
+const routes = require('./routes');
 
 // set up some configs for express.
 const config = {
@@ -27,71 +20,15 @@ const app = express();
 // create a logger object.  Using logger is preferable to simply writing to the console.
 const logger = log({ console: true, file: false, label: config.name });
 
+// specify middleware to use
 app.use(bodyParser.json());
 app.use(cors({
   origin: '*'
 }));
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-// Attempting to connect to the database.
-connection.connect(function (err) {
-  if (err) {
-    logger.error("Cannot connect to DB!");
-  } else {
-    logger.info("Connected to the DB!");
-  }
-});
-
-// GET /
-app.get('/', (req, res) => {
-  res.status(200).send('Go to 0.0.0.0:3000.');
-});
-
-
-// POST /reset
-app.post('/reset', (req, res) => {
-  connection.query('drop table if exists test_table', function (err, rows, fields) {
-    if (err)
-      logger.error("Can't drop table");
-  });
-  connection.query('CREATE TABLE `db`.`test_table` (`id` INT NOT NULL AUTO_INCREMENT, `value` VARCHAR(45), PRIMARY KEY (`id`), UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);', function (err, rows, fields) {
-    if (err)
-      logger.error("Problem creating the table test_table");
-  });
-  res.status(200).send('created the table');
-});
-
-// POST /multplynumber
-app.post('/multplynumber', (req, res) => {
-  console.log(req.body.product);
-
-  connection.query('INSERT INTO `db`.`test_table` (`value`) VALUES(\'' + req.body.product + '\')', function (err, rows, fields) {
-    if (err){
-      logger.error("Problem inserting into test table");
-    }
-    else {
-      res.status(200).send(`added ${req.body.product} to the table!`);
-    }
-  });
-});
-
-// GET /checkdb
-app.get('/values', (req, res) => {
-  connection.query('SELECT value FROM `db`.`test_table`', function (err, rows, fields) {
-    if (err) {
-      logger.error("Error while executing Query: \n", err);
-      res.status(400).json({
-        "data": [],
-        "error": "MySQL error"
-      })
-    }
-    else{
-      res.status(200).json({
-        "data": rows
-      });
-    }
-  });
-});
+//include routes
+routes(app, logger);
 
 // connecting the express object to listen on a particular port as defined in the config object.
 app.listen(config.port, config.host, (e) => {
