c9d125588129d002aea0fa1a818e5096b978fc98
CPetersSMU <cwpeters@smu.edu>
2021-10-28 17:44:37 -0500

    created conversations.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Conversations from './Conversations';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Conversations />, document.getElementById('convoTemp'));
