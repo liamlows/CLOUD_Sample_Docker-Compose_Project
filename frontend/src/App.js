ef61690015b12bc47d77faa058cb6de270369555
Christopher Deng Jr <cdengjr@smu.edu>
2021-11-02 20:09:46 -0500

    Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Homepage from './components/Homepage';
+
 
 // React functional component
 function App () {
@@ -70,18 +72,7 @@ function App () {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={fetchBase} style={{marginBottom: '1rem'}}> {`GET: http://${url}:8000/`} </button>
-        <button onClick={reset}> Reset DB </button>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={number} onChange={handleChange}/>
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          { values.map((value, i) => <li key={i}>{value.value}</li>) }
-        </ul>
-      </header>
+      <Homepage />
     </div>
   );
 }
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Homepage(){
+    return (
+        <>
+        <h1>Sports League</h1>
+        <h2>What league do you want to explore?</h2>
+        <ul>
+            <li>Leauge A</li>
+            <li>Leauge B</li>
+            <li>League C</li>
+            <li>League D</li>
+        </ul>
+      </>
+    )
+}
+
+export default Homepage;
