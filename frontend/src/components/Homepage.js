8add45c3ef237ee58112bf84a5ff317d9ffd03e7
Christopher Deng Jr <cdengjr@smu.edu>
2021-11-02 20:25:49 -0500

    Update Homepage.js

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -6,11 +6,10 @@ function Homepage(){
         <h1>Sports League</h1>
         <h2>What league do you want to explore?</h2>
         <div>
-            <img src="https://upload.wikimedia.org/wikipedia/en/0/03/National_Basketball_Association_logo.svg   "></img>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/03/National_Basketball_Association_logo.svg"></img>
         </div>
-
         <div>
-        <img src="https://upload.wikimedia.org/wikipedia/en/a/a2/National_Football_League_logo.svg"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/a2/National_Football_League_logo.svg"></img>
         </div>
         <div>
             <img src="https://upload.wikimedia.org/wikipedia/en/a/a6/Major_League_Baseball_logo.svg"></img>
