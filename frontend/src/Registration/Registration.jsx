71dc3f5186355a8443a4ac5861dcb961a96ac1a0
yunchengzhang1 <yunchengz@smu.edu>
2021-11-03 17:08:44 -0500

    Create Registration.jsx

---
+++ frontend/src/Registration/Registration.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export class AccountEditor extends React.Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: ''
+    };
+
+    
+
+    render() {
+        return <>
+        <form className="container">
+                <h1>Account Registration</h1>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input type="text"
+                        id="name"
+                        name="name"
+                        value={this.state.name}
+                        onChange={ event => this.setState({ name: event.target.value }) }
+                        className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="text"
+                        id="email"
+                        name="email"
+                        value={this.state.email}
+                        onChange={ event => this.setState({ email: event.target.value }) }
+                        className="form-control" />
+                </div>
+
+
+                <div className="form-group">
+                    <label htmlFor="password">Password (8 characters minimum): </label>
+                    <input type="password"
+                        id="pass"
+                        name="password"
+                        minlength="8" required
+                        value={this.state.password}
+                        onChange={ event => this.setState({ password: event.target.value }) }
+                        className="form-control" />
+                </div>
+
+            </form>
+        </>;
+    }
+}
