bb20421cccf89952d0832bdf89d584c32947252f
Christopher Deng Jr <cdengjr@smu.edu>
2021-11-09 19:16:23 -0600

    Using Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import Homepage from './components/Homepage';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Homepage from './components/Homepage'
 
 
-// React functional component
-function App () {
-  // state for storage of the information on the webpage of forms and list, uses hooks
-  const [number, setNumber] = useState("")
-  const [values, setValues] = useState([])
-
-  // ENTER YOUR EC2 PUBLIC IP/URL HERE
-  const ec2_url = ''
-  // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-  const ec2 = false;
-  // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-  const url = ec2 ? ec2_url : 'localhost'
-
-  // handle input field state change
-  const handleChange = (e) => {
-    setNumber(e.target.value);
-  }
-
-  const fetchBase = () => {
-    axios.get(`http://${url}:8000/`).then((res)=>{
-      alert(res.data);
-    })
-  }
-
-  // fetches vals of db via GET request
-  const fetchVals = () => {
-    axios.get(`http://${url}:8000/values`).then(
-      res => {
-        const values = res.data.data;
-        console.log(values);
-        setValues(values)
-    }).catch(err => {
-      console.log(err)
-    });
-  }
-
-  // handle input form submission to backend via POST request
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let prod = number * number;
-    axios.post(`http://${url}:8000/multplynumber`, {product: prod}).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-    setNumber("");
-  }
-
-  // handle intialization and setup of database table, can reinitialize to wipe db
-  const reset = () => {
-    axios.post(`http://${url}:8000/reset`).then(res => {
-      console.log(res);
-      fetchVals();
-    }).catch(err => {
-      console.log(err)
-    });;
-  }
-
-  // tell app to fetch values from db on first load (if initialized)
-  // the comment below silences an error that doesn't matter.=
-  useEffect(() => {
-    fetchVals();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  return (
+const App = props => {
+  return(
     <div className="App">
-      <Homepage />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
