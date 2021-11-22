312d5ce4db6dee463f0623b1e6c188b97ca1cd6b
Brigitta Pulins <bpulins@smu.edu>
2021-11-22 15:03:59 -0600

    test

--- backend/routes.js
+++ backend/routes.js
@@ -106,14 +106,14 @@ module.exports = function routes(app, logger) {
   * DRIVERS
   */
   
-  //GET all users
-  // /api/users
-  app.get('/users', function (req, res) {
-    pool.query("SELECT * FROM users", function (err, result, fields) {
-      if (err) throw err;
-      res.end(JSON.stringify(result)); // Result in JSON format
-    });
-  });
+  // //GET all users
+  // // /api/users
+  // app.get('/users', function (req, res) {
+  //   pool.query("SELECT * FROM users", function (err, result, fields) {
+  //     if (err) throw err;
+  //     res.end(JSON.stringify(result)); // Result in JSON format
+  //   });
+  // });
 
   //GET all drivers
   // /api/drivers
