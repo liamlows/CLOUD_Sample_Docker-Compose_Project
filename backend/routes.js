18a24e1a1a856b361c60adeb7e61ab881c62e2cb
Will Strickland <wstrickland@smu.edu>
2021-11-25 19:52:49 -0600

    get routes for player position, number, and picture

--- backend/routes.js
+++ backend/routes.js
@@ -814,5 +814,102 @@ module.exports = function routes(app, logger) {
                           });
 
 
+                          app.get('/player/number', async (req, res) => {
+                            // obtain a connection from our pool of connections
+                            pool.getConnection(function (err, connection){
+                              if (err){
+                                console.log(connection);
+                                // if there is an issue obtaining a connection, release the connection instance and log the error
+                                logger.error('Problem obtaining MySQL connection', err)
+                                res.status(400).send('Problem obtaining MySQL connection'); 
+                              }  else {
+                                    var playerID = req.param('playerID');
+                                  
+                                    
+                                    connection.query("select PlayerNumber from Players where PlayerID=?",playerID, function (err, result, fields) {
+                                      if (err) { 
+                                        // if there is an error with the query, release the connection instance and log the error
+                                        connection.release()
+                                        logger.error("Problem getting games from league: ", err);
+                                        res.status(400).send('Problem getting games from league'); 
+                                      } else { 
+                                        // if there is no error with the query, release the connection instance
+                                        res.send(result);
+                                        connection.release()
+                                        
+                                      }
+                                    });
+                                    
+                                  }
+                                });
+            
+            
+                              });
+
+                              app.get('/player/position', async (req, res) => {
+                                // obtain a connection from our pool of connections
+                                pool.getConnection(function (err, connection){
+                                  if (err){
+                                    console.log(connection);
+                                    // if there is an issue obtaining a connection, release the connection instance and log the error
+                                    logger.error('Problem obtaining MySQL connection', err)
+                                    res.status(400).send('Problem obtaining MySQL connection'); 
+                                  }  else {
+                                        var playerID = req.param('playerID');
+                                      
+                                        
+                                        connection.query("select Position from Players where PlayerID=?",playerID, function (err, result, fields) {
+                                          if (err) { 
+                                            // if there is an error with the query, release the connection instance and log the error
+                                            connection.release()
+                                            logger.error("Problem getting games from league: ", err);
+                                            res.status(400).send('Problem getting games from league'); 
+                                          } else { 
+                                            // if there is no error with the query, release the connection instance
+                                            res.send(result);
+                                            connection.release()
+                                            
+                                          }
+                                        });
+                                        
+                                      }
+                                    });
+                
+                
+                                  });
+
+                                  app.get('/player/picture', async (req, res) => {
+                                    // obtain a connection from our pool of connections
+                                    pool.getConnection(function (err, connection){
+                                      if (err){
+                                        console.log(connection);
+                                        // if there is an issue obtaining a connection, release the connection instance and log the error
+                                        logger.error('Problem obtaining MySQL connection', err)
+                                        res.status(400).send('Problem obtaining MySQL connection'); 
+                                      }  else {
+                                            var playerID = req.param('playerID');
+                                          
+                                            
+                                            connection.query("select Picture from Players where PlayerID=?",playerID, function (err, result, fields) {
+                                              if (err) { 
+                                                // if there is an error with the query, release the connection instance and log the error
+                                                connection.release()
+                                                logger.error("Problem getting games from league: ", err);
+                                                res.status(400).send('Problem getting games from league'); 
+                                              } else { 
+                                                // if there is no error with the query, release the connection instance
+                                                res.send(result);
+                                                connection.release()
+                                                
+                                              }
+                                            });
+                                            
+                                          }
+                                        });
+                    
+                    
+                                      });
+
+
 }
 
