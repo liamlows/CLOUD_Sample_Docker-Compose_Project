6a47eb3c307ce626c0f52b912b1c336d6651f937
tcy98 <407277971ctang@gmail.com>
2021-11-27 19:24:49 -0600

    update on api
    still have the weirdest problem

--- frontend/src/components/LeaguePage.jsx
+++ frontend/src/components/LeaguePage.jsx
@@ -8,38 +8,63 @@ import Container from "react-bootstrap/Container"
 import Stack from 'react-bootstrap/Stack'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { Card, CardGroup } from 'react-bootstrap'
+import { SportRepository } from '../api/SportRepository';
 
 
 
-const LeaguePage = (props) => {
-
-    const [games, setGames] = useState([]);
-    let league_name = props.league
-    const getAllGames = () => {
-        console.log("here");
-        console.log(props.league);
-        const leageu_name = 
-        axios.get(`http://${url}:8000/games/league`,{
-            params: {
-              league: league_name
-            }
-          })
-            .then((response) => {
-                console.log(response);
-                const allGames = response.data;
-                setGames(allGames);
-            })
-            .catch(err => {
-                console.log(err)
-            });
-
-            ;
+export class LeaguePage extends React.Component {
+    state = {
+        recent_games: []
+        
     }
 
-    useEffect(() => getAllGames(), []);
+    db = new SportRepository();
 
-    return (
-        <>
+    // async getTeamName2FromGameID(GameID) {
+    //     var team_name;
+    //     await this.db.getTeamName2FromGameID(GameID).then(
+    //         team2 => {
+    //             console.log("what is team2",team2);
+    //             team_name = team2[0].TeamName;
+                
+    //             console.log("what is team_name",team_name);
+
+    //             return team_name;
+    //         }
+    //     );
+    // }
+    
+    updateTeam2(){
+        var games = this.state.recent_games;
+        games.forEach(g=>{
+            this.db.getTeamName2FromGameID(g.GameID).then(x=> g.TeamName2 =x[0].TeamName);
+        });
+        this.setState({recent_games: games});
+    }
+
+    componentDidMount() {
+        console.log("didmount");
+        let league = this.props.league;
+        console.log("league : ", league);
+        if (league) {
+            this.db.getAllGames(league).then(games =>
+                this.setState({recent_games: games}, ()=>{
+                    console.log("Imhere",this.state.recent_games);
+                    // this.updateTeam2();
+                    games.forEach(g=>{
+                        this.db.getTeamName2FromGameID(g.GameID).then(x=> g.TeamName2 =x[0].TeamName);
+                    });
+                    this.setState({recent_games: games});
+                })
+            );
+        }
+
+    }
+
+
+    render() {
+        return <>
             <Navbar bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand>SportsTeamWebsite</Navbar.Brand>
@@ -57,22 +82,25 @@ const LeaguePage = (props) => {
                 <Row>
                     <Col>
                         <p> test </p>
-                        {games.map((games) => (
-                            <Stack direction="horizontal" gap={3}>
-                                <p>
-                                    {games.Team1ID} vs {games.Team2ID}
-                                </p>
-                            </Stack>
-                        ))}
+                        {console.log(this.state.recent_games)}
+                        <CardGroup>
+
+                            {this.state.recent_games.map((g) => (
+                                <Card>
+                                    <p>
+                                    {console.log("WTF",g.TeamName,g.TeamName2)}
+                                    {g.TeamName} vs {g.TeamName2} 
+                                    </p>
+                                </Card>
+                            ))}
+                        </CardGroup>
                     </Col>
                     <Col >
                     </Col>
                 </Row>
             </Container>
-
-
-        </>
-    )
+        </>;
+    }
 
 }
 
