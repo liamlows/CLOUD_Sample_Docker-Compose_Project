4ce81ea4a9d88799b09c1ac733bd19e659008309
Will Strickland <wstrickland@smu.edu>
2021-11-28 12:01:16 -0600

    Update url.js
    add ec2 url

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,6 +1,6 @@
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
-const ec2_url = ''
+const ec2_url = 'sampledockercompose.cr0a3sfmt8ny.us-east-2.rds.amazonaws.com'
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
 const ec2 = false;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
-export const url = ec2 ? ec2_url : 'localhost';
+export const url = ec2 ? ec2_url : 'ec2_url';
