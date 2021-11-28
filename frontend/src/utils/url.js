330c1ba71a3b7bab1d3df5fe2c4035fe84bbca83
Will Strickland <wstrickland@smu.edu>
2021-11-28 12:05:58 -0600

    Update url.js

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,6 +1,6 @@
 // ENTER YOUR EC2 PUBLIC IP/URL HERE
 const ec2_url = ''
 // CHANGE THIS TO TRUE IF HOSTING ON EC2, MAKE SURE TO ADD IP/URL ABOVE
-const ec2 = false;
+const ec2 = true;
 // USE localhost OR ec2_url ACCORDING TO ENVIRONMENT
 export const url = ec2 ? ec2_url : 'localhost';
