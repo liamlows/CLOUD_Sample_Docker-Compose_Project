7466b458b36755778297a9f60f7ced4dbe580e0c
Will Strickland <wstrickland@smu.edu>
2021-11-29 11:03:34 -0600

    get date of game given gameID

--- backend/routes.js
+++ backend/routes.js
@@ -1559,5 +1559,33 @@ app.put('/team/adCount', (req, res) => {
   });
 });
 
+//get game date given gameid
+app.get('/game/date', (req, res) => {
+  // obtain a connection from our pool of connections
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      console.log(connection);
+      // if there is an issue obtaining a connection, release the connection instance and log the error
+      logger.error('Problem obtaining MySQL connection', err)
+      res.status(400).send('Problem obtaining MySQL connection');
+    } else {
+      var gameID=req.param('gameID');
+      connection.query("select Date from Games where GameID=?",gameID, function (err, result, fields) {
+        if (err) {
+          // if there is an error with the query, release the connection instance and log the error
+          connection.release()
+          logger.error("Problem getting ppg: ", err);
+          res.status(400).send('Problem getting ppg');
+        } else {
+          // if there is no error with the query, release the connection instance
+          res.send(result);
+          connection.release()
+
+        }
+      });
+    }
+  });
+});
+
 }
 
