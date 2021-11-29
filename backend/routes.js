0eae565f68f01c35b9b9c9de42e972c8356e8fb4
Sydney Gibbs <sigibbs@smu.edu>
2021-11-28 20:34:42 -0600

    adding soup  kitchen routes

--- backend/routes.js
+++ backend/routes.js
@@ -500,6 +500,54 @@ module.exports = function routes(app, logger) {
       });
   });
 
+  /*
+  * SOUP KITCHEN ROUTES
+  */
+
+  //GET a list of all Soup Kitchens
+  // /api/soupkitchens
+  app.get('/api/soupkitchens', function (req, res) {
+    pool.query("SELECT * FROM soupKitchens", function (err, result, fields) {
+      if (err) throw err;
+      res.end(JSON.stringify(result)); // Result in JSON format
+    });
+  });
+
+  //GET a specific Soup Kitchen based on soupKitchenID
+  //	/api/soupkitchen
+  app.get('/api/soupkitchen', function (req, res) {
+    var soupKitchenID = req.param('soupKitchenID');
+    pool.query("SELECT * FROM soupKitchens WHERE soupKitchenID = ?", soupKitchenID, function (err, result, fields) {
+      if (err) throw err;
+      res.end(JSON.stringify(result)); // Result in JSON format
+    });
+  });
+
+  //POST a new soup kitchen
+  // /api/soupkitchens
+  app.post('/api/soupkitchens', async (req, res) => {
+    var soupKitchenID = req.param('soupKitchenID');
+    var userID = req.param('userID');
+    var soupKitchenName = req.param('soupKitchenName')
+    var address = req.param('address')
+
+    pool.query("INSERT INTO soupKitchens (soupKitchenID, userID, soupKitchenName, address) VALUES (?,?,?,?)", 
+    [soupKitchenID, userID, soupKitchenName, address],function (err, result, fields) {
+      if (err) throw err;
+      res.end(JSON.stringify(result)); // Result in JSON format
+    });
+  });
+
+    //DELETE a particular delivery
+  //  /api/soupkitchen
+  app.delete('/api/soupkitchen', async (req, res) => {
+    var soupKitchenID = req.param('soupKitchenID');
+    pool.query("DELETE FROM soupKitchens WHERE soupKitchenID = ?", soupKitchenID, function (err, result, fields) {
+      if (err) throw err;
+      res.end(JSON.stringify(result)); 
+      });
+  });
+
 
 //BRIGITTA'S ROUTES
 
