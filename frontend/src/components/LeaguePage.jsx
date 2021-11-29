4604e99beed26d76a2650e975c87919572b2acfc
Tony Tang <407277971ctang@gmail.com>
2021-11-28 20:12:30 -0600

    sortbydate

--- frontend/src/components/LeaguePage.jsx
+++ frontend/src/components/LeaguePage.jsx
@@ -8,7 +8,7 @@ import Container from "react-bootstrap/Container"
 import Stack from 'react-bootstrap/Stack'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import { Card, CardGroup } from 'react-bootstrap'
+import { Card, CardGroup, NavDropdown } from 'react-bootstrap'
 import { SportRepository } from '../api/SportRepository';
 
 
@@ -56,9 +56,9 @@ export class LeaguePage extends React.Component {
                         this.db.getTeamName2FromGameID(g.GameID).then(x => {
                             g["OpponentName"] = x[0].TeamName;
                             // g.OpponentName = x[0].TeamName;
-                            
+
                         }
-                            );
+                        );
                     });
                     this.setState({ recent_games: games });
                 })
@@ -71,11 +71,14 @@ export class LeaguePage extends React.Component {
 
             )
         }
-
-
-
     }
 
+    sortGamesbyDateASEC() {
+        this.setState(this.state.recent_games.sort((a, b) => a.Date > b.Date ? 1 : -1))
+    }
+    sortGamesbyDateDESC() {
+        this.setState(this.state.recent_games.sort((a, b) => a.Date < b.Date ? 1 : -1))
+    }
 
     render() {
         return <>
@@ -91,9 +94,30 @@ export class LeaguePage extends React.Component {
                 </Container>
             </Navbar>
 
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand>Recent Games</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            {/* <NavDropdown title="Sort by Score" id="basic-nav-dropdown">
+                                <NavDropdown.Item onClick={() => this.sortGamesbyDateASEC()}> Sort Oldest to Newest </NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => this.sortGamesbyDateDESC()}> Sort Newest to Oldest </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            </NavDropdown> */}
+                            <NavDropdown title="Sort by Date" id="basic-nav-dropdown">
+                                <NavDropdown.Item onClick={() => this.sortGamesbyDateASEC()}> Sort Oldest to Newest </NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => this.sortGamesbyDateDESC()}> Sort Newest to Oldest </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
 
             <Container>
-                <h2> Recent Games </h2>
+
                 <Row>
 
                     {console.log(this.state.recent_games)}
@@ -110,6 +134,7 @@ export class LeaguePage extends React.Component {
                                             {g.Team1Score} - {g.Team2Score}
                                         </Card.Text>
                                     </Card.Body>
+                                    <Card.Footer className="text-muted"> GameDate: {g.Date.slice(0, 10)}</Card.Footer>
                                 </Card>
                             </Col>
                         ))}
@@ -118,14 +143,14 @@ export class LeaguePage extends React.Component {
             </Container>
             <br />
             <Container>
-                <h3> Ranking in {this.props.league} </h3>
+                <h3 className="text-start"> Ranking in {this.props.league} </h3>
                 <Row>
                     <Col>
                         {this.state.ranking.map((r, idx) => (
 
                             <Card bg="secondary" key={idx} text="white">
                                 <Card.Body>
-                                    <Card.Title > #{idx+1} : {r.TeamName} </Card.Title>
+                                    <Card.Title > #{idx + 1} : {r.TeamName} </Card.Title>
                                     <Card.Text> Record : {r.Wins} - {r.Losses} </Card.Text>
                                 </Card.Body>
                             </Card>
