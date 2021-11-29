5ff4f82d735e8b4687511c87d704c5b670414f53
Christopher Deng Jr <cdengjr@smu.edu>
2021-11-29 00:04:32 -0600

    small fix

--- frontend/src/components/GameView.js
+++ frontend/src/components/GameView.js
@@ -158,7 +158,7 @@ export class GameView extends React.Component {
                     <Col>
                         <Form>
                             <Form.Group className="mb-3" >
-                                <Form.Control onChange={x => this.setState({ nameSearch: x.target.value })} placeholder="Search For Team 1 Player" />
+                                <Form.Control type="text" onChange={x => this.setState({ nameSearch: x.target.value })} placeholder="Search For Team 1 Player" />
                             </Form.Group>
                         </Form>
                     </Col>
@@ -172,7 +172,7 @@ export class GameView extends React.Component {
                     <Col>
                         <Form>
                             <Form.Group className="mb-3" >
-                                <Form.Control onChange={x => this.setState({ nameSearch: x.target.value })} placeholder="Search For Team 2 Player" />
+                                <Form.Control type="text" onChange={x => this.setState({ nameSearch: x.target.value })} placeholder="Search For Team 2 Player" />
                             </Form.Group>
                         </Form>
                     </Col>
