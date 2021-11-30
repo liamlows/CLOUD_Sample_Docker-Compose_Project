964a77073f6ae2c22bbc386903979b1bff340092
Landon Wood <landonw@smu.edu>
2021-11-30 04:39:59 -0600

    Improve ProfileTable functionality

--- frontend/src/Profile/ProfileTable.jsx
+++ frontend/src/Profile/ProfileTable.jsx
@@ -15,34 +15,44 @@ export class ProfileTable extends React.Component {
         return <>
             <h4>My Donations</h4>
             <div className="container table_container">
-                <table className="table table-bordered table-striped my-4">
-                    <thead>
-                        <tr>
-                            <th scope="col">Donation #</th>
-                            <th scope="col">RDH Donor</th>
-                            <th scope="col">Soup Kitchen Recipient</th>
-                            <th scope="col">Food</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Claimed</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.donations.map((x, i) => 
-                                <tr key={i}>
-                                        <th scope="row"><Link to={`/donation/`+ x.foodDonationID} >{x.foodDonationID}</Link></th>
-                                        <td>{x.RDH_name}</td>
-                                        <td>{x.soupKitchenID}</td> {/* WOuld probably have to update route to alias user ids. */}
-                                        <td>{x.foodName}</td>
-                                        <td>{x.quantity}</td>
-                                        {/* <td>{x.timeMade.substring(0, 10)}</td>
-                                        <td>{x.expirationDate.substring(0, 10)}</td> */}
-                                        <td>{x.claimed}</td>
+                
+                {
+                    !this.state.donations.length && 
+                        <div className="d-flex justify-content-center">
+                            You aren't associated with any food donations.
+                        </div>
+                }
+                {
+                    this.state.donations.length !== 0 &&
+                        <table className="table table-bordered table-striped my-4">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Donation #</th>
+                                    <th scope="col">RDH Donor</th>
+                                    <th scope="col">Soup Kitchen Recipient</th>
+                                    <th scope="col">Food</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Claimed</th>
                                 </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.donations.map((x, i) => 
+                                        <tr key={i}>
+                                                <th scope="row"><Link to={`/donation/`+ x.foodDonationID} >{x.foodDonationID}</Link></th>
+                                                <td>{x.RDH_name}</td>
+                                                <td>{x.soupKitchenID}</td> {/* WOuld probably have to update route to alias user ids. */}
+                                                <td>{x.foodName}</td>
+                                                <td>{x.quantity}</td>
+                                                {/* <td>{x.timeMade.substring(0, 10)}</td>
+                                                <td>{x.expirationDate.substring(0, 10)}</td> */}
+                                                <td>{x.claimed}</td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                }
             </div>
         </>;
     }
