afb9eb152243a9cb5c2333be21aed617b17afbab
ChrisA657 <54815428+ChrisA657@users.noreply.github.com>
2022-03-03 08:03:53 -0600

    package updates and auth start

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,86 +1,99 @@
-import { useState } from "react"
-import { render } from "react-dom"
+import { useState, useEffect } from "react";
 import './Login.css';
-import {account} from "../../api/account";
-const Login=()=>{
-    const[user, setUsername]= useState("");
-    const[password, setPassword]=useState("");
-    const[err, setErr]=useState('');
-    const[signUp, setSignUp]=useState(false);
-    const[rUser, setRUsername]= useState("");
-    const[email, setEmail]= useState("");
-    const[rPassword, setRPassword]=useState("");
-    const[cPassword, setCPassword]= useState("");
+import { account } from "../../api/account";
+const Login = () => {
+    const [user, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [err, setErr] = useState('');
+    const [signUp, setSignUp] = useState(false);
+    const [rUser, setRUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [rPassword, setRPassword] = useState("");
+    const [cPassword, setCPassword] = useState("");
     const api = new account();
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
+    useEffect(() => {
+        if (err) {
+            setTimeout(() => {
+                setErr("");
+            }, 4000);
+        }
 
-        if(signUp) {
-            if(rPassword !== cPassword){
-                setErr("passwords do not match");
-                return;
-            } else {
-                api.register(rUser,email,rPassword).then((res)=>{
-                    console.log(res);
-                }).catch((err)=>{
-                    setErr(err);
-                });
-            }
-            
-        } else {
-                api.login(user,password).then((res)=>{
-                     alert(res)
-                });
-            }    
-    }
-    const toggleSignUp = (e)=>{
+    }, [err]);
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        setSignUp(prev=>!prev);
+        if (signUp) {
+            if (rPassword !== cPassword) {
+                setErr("passwords do not match");
+            } else {
+                api.register(rUser, email, rPassword)
+                    .then((res) => {
+                        console.log(res);
+                        alert("successfully registered");
+                    })
+                    .catch((err) => {
+                        setErr(err);
+                        console.log(err);
+                    });
+            }
+
+        } else {
+            api.login(user, password).then((res) => {
+                alert(res)
+            }).catch((err)=>{
+                alert(err +"catch");
+            });
+        }
+        
     }
-    return(
+    const toggleSignUp = (e) => {
+        e.preventDefault();
+        setSignUp(prev => !prev);
+    }
+    return (
         <div className="login">
             <form>
                 <div className="login-container">
                     {
-                    !signUp ? 
-                        <>
-                            <div className="login-form-field">
-                                <label htmlFor="user">Username</label>
-                                <input type="text" value={user} name="user" id="user" onChange={e=>setUsername(e.target.value)}/>
-                            </div>
-                            
-                            <div className="login-form-field">
-                                <label htmlFor="password"> Password</label>
-                                <input type="text" value={password} name="user" id="password" onChange={e=>setPassword(e.target.value)}/>
-                            </div>
-                        </>
-                    :
-                        <>
-                            {err?<p>{err}</p>:''}
-                            <div className="login-form-field">
-                                <label htmlFor="user">Username</label>
-                                <input type="text" value={rUser} name="rUser" id="rUser" onChange={e=>setRUsername(e.target.value)}/>
-                            </div>
-                            <div className="login-form-field">
-                                <label htmlFor="email">Email</label>
-                                <input type="text" value={email} name="email" id="email" onChange={e=>setEmail(e.target.value)}/>
-                            </div>
-                            <div className="login-form-field">
-                                <label htmlFor="rPassword">Password</label>
-                                <input type="text" value={rPassword} name="rPassword" id="rPassword" onChange={e=>setRPassword(e.target.value)}/>
-                            </div>
-                            <div className="login-form-field">
-                                <label htmlFor="cPassword">Confirm Password</label>
-                                <input type="text" value={cPassword} name="cPassword" id="cPassword" onChange={e=>setCPassword(e.target.value)}/>
-                            </div>
-                                
-                            
+                        !signUp ?
+                            <>
+                                <div className="login-form-field">
+                                    <label htmlFor="user">Username</label>
+                                    <input type="text" value={user} name="user" id="user" onChange={e => setUsername(e.target.value)} />
+                                </div>
 
-                        </>
+                                <div className="login-form-field">
+                                    <label htmlFor="password"> Password</label>
+                                    <input type="text" value={password} name="user" id="password" onChange={e => setPassword(e.target.value)} />
+                                </div>
+                            </>
+                            :
+                            <>
+                                {err ? <p>{err}</p> : ''}
+                                <div className="login-form-field">
+                                    <label htmlFor="user">Username</label>
+                                    <input type="text" value={rUser} name="rUser" id="rUser" onChange={e => setRUsername(e.target.value)} />
+                                </div>
+                                <div className="login-form-field">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="text" value={email} name="email" id="email" onChange={e => setEmail(e.target.value)} />
+                                </div>
+                                <div className="login-form-field">
+                                    <label htmlFor="rPassword">Password</label>
+                                    <input type="text" value={rPassword} name="rPassword" id="rPassword" onChange={e => setRPassword(e.target.value)} />
+                                </div>
+                                <div className="login-form-field">
+                                    <label htmlFor="cPassword">Confirm Password</label>
+                                    <input type="text" value={cPassword} name="cPassword" id="cPassword" onChange={e => setCPassword(e.target.value)} />
+                                </div>
+
+
+
+                            </>
                     }
-                    <button type="submit" onClick={handleSubmit}>{signUp ? "Register":"Login"}</button>
-                    <button onClick={toggleSignUp}>{signUp ? "login instead":"sign up"}</button>
+                    <button type="button" onClick={handleSubmit}>{signUp ? "Register" : "Login"}</button>
+                    <button onClick={toggleSignUp}>{signUp ? "login instead" : "sign up"}</button>
                 </div>
             </form>
         </div>
--- frontend/src/api/account.js
+++ frontend/src/api/account.js
@@ -1,38 +1,47 @@
 import axios from "axios";
 const URL = "http://localhost:8000/api/account/"
 
-export class account{
-    login(user,password){
-        return axios
-        .post(URL +"login",{
-            user,password
-        }).then(res =>{
-            console.log(res);
-            return res;
-        }).catch(err=>{
-            if(err.response){
-                console.log(err);
-            return err.response.data;
-            }
-                
+export class account {
+    login(user, password) {
+        return new Promise((resolve,reject)=>{
+            axios
+            .post(URL + "login", {
+                user, password
+            }).then(res => {
+                resolve(res);
+            }).catch(err => {
+                if (err.response) {
+                    console.log(err);
+                    reject(err.response);
+                } else if (err.request){
+                    reject("Server didn't respond");
+                } else {
+                    reject("Request failed");
+                }
+            })
+        })    
+    }
+
+    register(rUser, email, rPassword) {
+        return new Promise((resolve,reject) =>{
+            axios.post(URL + "register", {
+                rUser, email, rPassword
+            }).then(res => {
+                console.log(res);
+                resolve(res);
+            }).catch(err => {
+                if (err.response) {
+                    console.log(err);
+                    reject(err.response)
+                } else if (err.request){
+                    reject("Server didn't respond");
+                } else {
+                    reject("Request failed");
+                }
+
+            })
         })
     }
 
-    register(rUser,email,rPassword){
-        return axios
-        .post(URL +"register",{
-            rUser,email,rPassword
-        }).then(res =>{
-            console.log(res);
-            return res;
-        }).catch(err=>{
-            if(err.response){
-                console.log(err);
-            return Promise.reject(err.response.data);
-            }
-                
-        })
-    }
-    
 }
 
