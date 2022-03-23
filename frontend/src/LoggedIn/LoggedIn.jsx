5ebdafab8eef29fd9aebad606fc3a203474e7f3f
wesoa012 <wesa@smu.edu>
2022-03-23 14:42:07 -0500

    Takes you back to base page after signing out

--- frontend/src/LoggedIn/LoggedIn.jsx
+++ frontend/src/LoggedIn/LoggedIn.jsx
@@ -3,6 +3,6 @@ import { GenericButton } from "../common/GenericButton"
 export const LoggedIn = () => {
 
     return <section id="loggedInView">
-        <GenericButton label="Sign Out" click="/login" />
+        <GenericButton label="Sign Out" click="/" />
     </section>
 }
