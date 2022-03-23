f8e7abca1fc3367637ad5033821b8e7a8ebf47cd
katebouis <78283299+katebouis@users.noreply.github.com>
2022-03-23 18:58:12 -0500

    getting rid of default App values

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+// React functional component
+function App () {
+  
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
