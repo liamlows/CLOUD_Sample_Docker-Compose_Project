3bd83971cd4c52e42f7a65134b9cb31874e1deec
wesoa012 <wesa@smu.edu>
2022-03-23 13:18:45 -0500

    Generic Button now loads a page on click, In future need to change(for none transfer buttons)

--- frontend/src/common/GenericButton.jsx
+++ frontend/src/common/GenericButton.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom"
+
 export const GenericButton = ({ label, click}) => {
+    const navigate = useNavigate();
+    const path = click;
     return <div className="form-group">
-        <button type="button" onClick={click}>{ label }</button>
+        <button type="button" onClick={() => navigate(path) }>{ label }</button>
     
 </div>
 }
