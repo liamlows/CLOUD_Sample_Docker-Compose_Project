f02791cf8991efb575185069c5cf6eddfc40617d
wesoa012 <wesa@smu.edu>
2022-03-22 22:47:06 -0500

    allow page to change views

---
+++ frontend/src/viewer.js
@@ -0,0 +1,12 @@
+(app => {
+    
+    app._changeView = viewId => {
+        let sections = document.querySelectorAll('main > section');
+        sections.forEach(section => {
+            section.classList.remove('active');
+        });
+
+        document.getElementById(viewId).classList.add('active');
+    };
+
+})(app || (app = {}));
