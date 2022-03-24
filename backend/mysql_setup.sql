1882755248aa9bee83a91a7030b3f440bb149b9f
njding <55780724+njding@users.noreply.github.com>
2022-03-23 19:59:05 -0500

    Finished Tables for Student Enrollment and Waitlist

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -15,4 +15,25 @@ CREATE TABLE `db`.`test_table` (
 -- insert sample entry
 INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
 
+-- create table in db
+CREATE TABLE 'db'.'StudentEnrollment' (
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    'studentID' VARCHAR NOT NULL,
+    'schoolID' VARCHAR NOT NULL, 
+    FOREIGN KEY('studentID') REFERENCES Student('id') 
+    FOREIGN KEY('schoolID') REFERENCES School('id') 
+    PRIMARY KEY (`id`), 
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
 
+-- create table in db
+CREATE TABLE 'db'.'Waitlist' (
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    'studentID' VARCHAR NOT NULL,
+    'schoolID' VARCHAR NOT NULL, 
+    'timestamp' datetime NOT NULL,
+    FOREIGN KEY('studentID') REFERENCES Student('id') 
+    FOREIGN KEY('schoolID') REFERENCES School('id') 
+    PRIMARY KEY (`id`), 
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
