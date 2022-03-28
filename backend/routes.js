f383475665b3c19baf23d4a7bd7745557bd1cca9
alexanderr <localtoasted@pm.me>
2022-03-28 00:38:47 -0500

    add persistent login sessions and logout
    sessions are stored in mysql

--- backend/routes.js
+++ backend/routes.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // GET /
 router.get('/', (req, res) => {
-  res.status(200).send('Hello world.');
+  res.status(200);
+
+  if(req.session.username){
+    res.write(`Welcome, ${req.session.username}.`);
+  } else{
+    res.write('You are not currently logged in.');
+  }
+
+  res.send();
 });
 
 module.exports = router;
