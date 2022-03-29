c1e950f07ee5fe7af82a5e17454b0c8232e9e404
wesoa012 <wesa@smu.edu>
2022-03-29 17:39:22 -0500

    Now able to click the login button

--- frontend/src/Login/LoginPage.jsx
+++ frontend/src/Login/LoginPage.jsx
@@ -1,10 +1,28 @@
 import { PasswordField, TextField } from "../common";
 import { useState } from "react";
 import { GenericButton } from "../common/GenericButton";
+import { logIntoAccount } from "../APIFolder/loginApi";
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 
 
 export const LoginPage = () => {
+    const navigate = useNavigate();
+    const checkIfLoginSucc = (response) => {if(response.success === 1){
+        navigate(`/users/${Cookies.get("username")}`);
+    }else{
+        window.alert("Fail");
+    };}
+    
 
+
+    const login = (username, password) => {
+
+
+        logIntoAccount({"username": username, "password": password}).then(response => checkIfLoginSucc(response)).catch(console.log("boner biter"));
+        // console.log(response);
+        
+    }
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -21,7 +39,14 @@ export const LoginPage = () => {
                 value={password}
                 setValue={x => setPassword(x)} />
 
-            <GenericButton label="Login" click="/loggedIn" />
+            <button 
+            type="button" 
+            onClick={() => login(username,password)}
+            variant="contained" 
+            color="success">
+                Login
+            </button>
+            {/* <GenericButton label="Login" click="/loggedIn" /> */}
             <p className="mb-0">or</p>
             <GenericButton label="Sign Up" click="/signUp" />
         </form>
