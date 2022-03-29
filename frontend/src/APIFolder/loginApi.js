4930d4e8d2de6ba77e32bec51d99796f5309d915
wesoa012 <wesa@smu.edu>
2022-03-29 17:26:56 -0500

    Adding api for login/sign up

---
+++ frontend/src/APIFolder/loginApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+
+export const addAccount = (account) => new Promise((resolve, reject) => {
+    console.log(account)
+    axios.post('http://localhost:8000/account/register', account)
+        .then(x => resolve(x.data))
+        .catch(x => {
+            alert(x);
+            reject(x);
+        });
+});
+export const logIntoAccount = (account) => new Promise((resolve, reject) => {
+    console.log(account)
+    axios.post('http://localhost:8000/account/login', account)
+        .then(x => resolve(x.data))
+        .catch(x => {
+            // alert(x);
+            reject(x);
+        });
+});
