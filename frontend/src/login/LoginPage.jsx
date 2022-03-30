0b0258942c587f8464e8dfa677ea40006fa55ee9
katebouis <78283299+katebouis@users.noreply.github.com>
2022-03-30 13:35:54 -0500

    starting login page

--- frontend/src/login/LoginPage.jsx
+++ frontend/src/login/LoginPage.jsx
@@ -1,7 +1,12 @@
+import { TextField } from "@mui/material";
 import { useState } from "react";
 
 export const LoginPage = () => {
-    return(
-    <div>login</div>
-    );
+    return(<>
+    <h1>Log Into Your Account</h1>
+    <TextField helperText="Please enter your username" id="demo-helper-text-misaligned" label="Username"/>
+    <br/>
+    <br/>
+    <TextField helperText="Please enter your password" id="demo-helper-text-misaligned" label="Password"/>
+    </>);
 }
