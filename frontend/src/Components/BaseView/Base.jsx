411a543ec6bf21d2fe14a0b9ee866551cb72c23c
wesoa012 <wesa@smu.edu>
2022-04-05 08:30:38 -0500

    Homepage is now under baseview

--- frontend/src/Components/BaseView/Base.jsx
+++ frontend/src/Components/BaseView/Base.jsx
@@ -5,26 +5,30 @@ import Button from "@mui/material/Button";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { HomeView } from "../LoggedIn/HomeView";
+import BaseResponsiveAppBar from "../common/BaseResponsiveAppBar";
 
-export const Base = ({ currUser, setCurrUser }) => {
+export const Base = ({ currUser, setCurrUser, basePages, loggedInPages, settings}) => {
 
     const navigate = useNavigate();
 
     //if logged in (will store across refreshes) - May want to use local storage instead of cookie creation
-
     useEffect(() => {
         if (Cookies.get("username")) {
             setCurrUser(Cookies.get("username"));
-            navigate('/home');
         }
-        else{
+        else {
             setCurrUser('');
         }
     }, [currUser]);
 
     return <section className="baseView">
-        <h1 className="mb-4">Welcome</h1>
-        <h2 className="">This is the base page to be updated with logo and stuff</h2>
-        <GenericButton label="Login" click="/login" variant="contained" color="success" />
+        {currUser === '' && <div>
+            <BaseResponsiveAppBar pages={basePages} signIn={navigate('/login')} signUp={navigate('/signUp')}/>
+            <h1 className="mb-4">Welcome</h1>
+            <h2 className="">This is the base page to be updated with logo and stuff</h2>
+            <GenericButton label="Login" click="/login" variant="contained" color="success" />
+        </div>}
+        {currUser !== '' && <HomeView currUser={currUser} setCurrUser={x => setCurrUser(x)} pages={loggedInPages} settings={settings} />}
     </section>
 }
