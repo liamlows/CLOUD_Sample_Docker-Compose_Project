f37ea669a9d599e66db8d1d807f7c468f51369e9
wesoa012 <wesa@smu.edu>
2022-04-04 19:57:57 -0500

    Changing profile page to return based on what is in effect

--- frontend/src/Components/Profiles/Profile.jsx
+++ frontend/src/Components/Profiles/Profile.jsx
@@ -1,6 +1,6 @@
-import { getEmailbyUsername, getFirstNamebyUsername, getLastNamebyUsername, updateAccount} from "../APIFolder/loginApi";
+import { getEmailbyUsername, getFirstNamebyUsername, getLastNamebyUsername, updateAccount, getAccountbyUsername } from "../../APIFolder/loginApi";
 import { TextField } from "../common";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
@@ -11,11 +11,15 @@ export const Profile = () => {
     let editMode = false;
 
     //Doesn't currently know what info to get from the database
-    const [firstName, setFirstName] = useState(getFirstNamebyUsername(username));
-    const [lastName, setLastName] = useState(getLastNamebyUsername(username));
-    const [email, setEmail] = useState(getEmailbyUsername(username));
+    const [account, setAccount] = useState(undefined)
+    // const [firstName, setFirstName] = useState(getFirstNamebyUsername(username));
+    // const [lastName, setLastName] = useState(getLastNamebyUsername(username));
+    // const [email, setEmail] = useState(getEmailbyUsername(username));
     const username = Cookies.get("username");
 
+    useEffect(() => {
+        getAccountbyUsername({ loadedUser }).then(x => setAccount(x));
+    }, [startEditing]);
 
     const startEditing = () => {
         editMode = true;
@@ -23,55 +27,45 @@ export const Profile = () => {
     }
     const doneEditing = () => {
         editMode = false;
-        updateAccount({"username": username,"firstName": firstName, "lastName": lastName, "email": email});
+        updateAccount({ "username": account.username, "firstName": account.firstName, "lastName": account.lastName, "email": account.email });
         navigate(`/${username}`)
     }
 
+    const changeFirstName = delta => setAccount({...account, ...delta});
 
+    const changeLastName = delta => setAccount({...account, ...delta});
+    const changeEmail = delta => setAccount({...account, ...delta});
     // Basically check if user is the same user as the loaded profile.
     // If so then allow them to edit with the edit button at the end (this edit button will update the database once done)
     // If not then display the profile without the edit buttons.
 
     // NOTE - IN FUTURE ADD BUTTON TO SEND FRIEND REQUEST...ONLY IF FUNCTIONALITY IS IMPLEMENTED
 
-    if(username === loadedUser){//Need to get loadedUser from somewhere, probably button click to get into profile
-        if(editMode)
-        {
-            
-            return <section className="userProfile">
-                <h1>{loadedUser}'s Profile</h1>
-                <TextField label="First Name :" value={firstName} setValue={setFirstName} />
-                <TextField label="Last Name :" value={lastName} setValue={setLastName}/>
-                <TextField label="Email :" value={email} setValue={setEmail} />
-                <button onClick={doneEditing()}>Save</button>
-            </section>
-        }
-        else{
-
-            return <section className="userProfile">
-                <h1>{loadedUser}'s Profile</h1>
-                <h2>First Name :</h2>
-                <p>{firstName}</p>
-                <h2>Last Name :</h2>
-                <p>{lastName}</p>
-                <h2>Email :</h2>
-                <p>{email}</p>
-                <button onClick={startEditing()}>Edit Profile</button>
-            </section>
-        }
-    }
-    else
-    {
-        return <section className="userProfile">
-                <h1>{loadedUser}'s Profile</h1>
-                <h2>First Name :</h2>
-                <p>{firstName}</p>
-                <h2>Last Name :</h2>
-                <p>{lastName}</p>
-                <h2>Email :</h2>
-                <p>{email}</p>
-            </section>
-    }
-
-
+    return <section className="userProfile">
+        {account.username === loadedUser && editMode && <div>
+            <h1>{loadedUser}'s Profile</h1>
+            <TextField label="First Name :" value={account.firstName} setValue={x => changeFirstName(x)} />
+            <TextField label="Last Name :" value={account.lastName} setValue={x => changeLastName(x)} />
+            <TextField label="Email :" value={account.email} setValue={x => changeEmail(x)} />
+            <button onClick={doneEditing()}>Save</button>
+        </div>}
+        {account.username === loadedUser && !editMode && <div> <h1>{loadedUser}'s Profile</h1>
+            <h2>First Name :</h2>
+            <p>{account.firstName}</p>
+            <h2>Last Name :</h2>
+            <p>{account.lastName}</p>
+            <h2>Email :</h2>
+            <p>{account.email}</p>
+            <button onClick={startEditing()}>Edit Profile</button>
+        </div>}
+        {account.username !== loadedUser && <div>
+            <h1>{loadedUser}'s Profile</h1>
+            <h2>First Name :</h2>
+            <p>{account.firstName}</p>
+            <h2>Last Name :</h2>
+            <p>{account.lastName}</p>
+            <h2>Email :</h2>
+            <p>{account.email}</p>
+        </div>}
+    </section>
 }
