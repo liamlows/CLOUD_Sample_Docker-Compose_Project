d19598b678a5255d02229ebf85d947b1eecfa5fc
wesoa012 <wesa@smu.edu>
2022-04-05 00:06:56 -0500

    HomeView has a working nav bar at the top.
    Need to add the actual views that they go to and the routing that they go to, but the base line is working

--- frontend/src/Components/LoggedIn/HomeView.jsx
+++ frontend/src/Components/LoggedIn/HomeView.jsx
@@ -5,26 +5,38 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import ResponsiveAppBar from "./ResponsiveAppBar";
 import Cookies from "js-cookie";
+import { useState } from "react";
+import { getAccountbyUsername } from "../../APIFolder/loginApi";
 
-export const HomeView = () => {
+export const HomeView = (props) => {
 
     const navigate = useNavigate();
 
-    const pages = ['Classes'];
-    const settings = ['Public Profile', 'Account', 'Dashboard', 'Logout'];
+    const [ account, setAccount ] = useState(undefined);
+
+    if(!account){
+        getAccountbyUsername(props.username).then(x => setAccount(x));
+    }
+
+    const signOut = () => {
+        Cookies.remove("username");
+        Cookies.remove("connect.sid");
+        console.log(Cookies.get());
+        navigate('/');
+    }
+
+    const [pages] = useState(['Classes']);
+    const [settings] = useState([
+        // {label: 'Public Profile', route: `/users/${account.id}` }, Keep out until have an account id confirmed
+        // { label: 'Account', route: `/accounts/${account.id}` }, 
+        {label: 'Public Profile', route: `/users` }, 
+        { label: 'Account', route: `/accounts` }, 
+        { label: 'Dashboard', route: '/home' },
+        // { label: 'Logout', onclick: 'signOut()'}
+    ]);
 
     return <div>
         <ResponsiveAppBar pages={pages} settings={settings}></ResponsiveAppBar>
-        <h1 className="mb-4">Welcome {Cookies.get("username")}</h1>
-        <h2 className="">Mega Yuh</h2>
-        <Link className="btn btn-secondary col-1" to='/'>
-            Sign Out
-        </Link>
-        <button type="button" onClick={() => {
-            Cookies.remove("username");
-            Cookies.remove("connect.sid");
-            console.log(Cookies.get());
-            navigate('/');
-        }}>Sign Out</button>
+        <h1 className="mb-4">Welcome {props.username}</h1>
     </div>
 }
