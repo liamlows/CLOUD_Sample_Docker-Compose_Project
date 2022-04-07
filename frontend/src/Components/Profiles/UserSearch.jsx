95173863ef192b4105d0121f035402e360a71778
wesoa012 <wesa@smu.edu>
2022-04-07 01:19:49 -0500

    Fully working search. Navigation to user is still jacked up

--- frontend/src/Components/Profiles/UserSearch.jsx
+++ frontend/src/Components/Profiles/UserSearch.jsx
@@ -1,6 +1,7 @@
 import { accordionActionsClasses } from "@mui/material";
 import { useEffect } from "react";
 import { useState } from "react"
+import { findDOMNode } from "react-dom";
 import { Navigate, useNavigate } from "react-router-dom";
 import { getProfiles } from "../../APIFolder/loginApi"
 import { TextField } from "../common";
@@ -10,9 +11,10 @@ import LoggedInResponsiveAppBar from "../common/LoggedInResponsiveAppBar";
 export const UserSearch = ({ currUser, setCurrUser, setLoadedUser, }) => {
 
     const navigate = useNavigate();
+    // const [allProfiles, setProfiles] = useState(undefined);
     const [profiles, setProfiles] = useState(undefined);
 
-    const [username, setUsername] = useState(undefined);
+    const [username, setUsername] = useState('');
 
     const goToProfile = profile => {
         setLoadedUser(profile);
@@ -23,14 +25,22 @@ export const UserSearch = ({ currUser, setCurrUser, setLoadedUser, }) => {
         navigate(`/users/${currUser.username}/friends`);
     }
 
-    useEffect(() => {
-        getProfiles().then(response => { setProfiles(response) }).then(console.log("recieved users info"));
-    }, []);
+    useEffect(() => {}, [username]);
 
-    // if (!accounts) {
-    //     getProfiles().then(response => { setProfiles(response) })
-    //     return <>Loading...</>
-    // }
+    const find = profile => {
+        let x = profile.username
+        // console.log(typeof(x))
+        if(x.search(username) !==-1){return true;}
+        else{return false;}
+
+        
+        // console.log(x.search(username));
+    }
+
+    if (!profiles) {
+        getProfiles().then(response => setProfiles(response) && console.log("recieved users info"));
+        return <>Loading...</>
+    }
 
     return <div>
         {/* <LoggedInResponsiveAppBar/> To implement */}
@@ -50,16 +60,17 @@ export const UserSearch = ({ currUser, setCurrUser, setLoadedUser, }) => {
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Friend Count</th>
-                    <th>&nbsp;</th>
+                    <th className="col-2"></th>
                 </tr>
             </thead>
             <tbody>
-                {profiles && profiles.map(profile => <tr className="container">
+                {profiles && profiles.map(profile => find(profile) && <tr key={profile.username} className="container">
+                    
                     <td>{profile.username}</td>
                     <td>{profile.firstName}</td>
                     <td>{profile.lastName}</td>
                     <td className="ts-2">0</td>
-                    <td><button type="button" className="btn btn-secondary" onClick={profile => console.log(profile) && goToProfile(profile)}>View Profile</button></td>
+                    <td><button type="button" className="btn btn-secondary" onClick={profile => goToProfile(profile)}>View Profile</button></td>
                 </tr>)}
             </tbody>
         </table>
