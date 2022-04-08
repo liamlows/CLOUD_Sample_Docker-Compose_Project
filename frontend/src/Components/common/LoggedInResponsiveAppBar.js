f884bb45e6f4d750ff8245cdcdc6930e05a4fbc5
wesoa012 <wesa@smu.edu>
2022-04-08 14:04:39 -0500

    not using avatar

--- frontend/src/Components/common/LoggedInResponsiveAppBar.js
+++ frontend/src/Components/common/LoggedInResponsiveAppBar.js
@@ -7,7 +7,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
