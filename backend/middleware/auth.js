482561785635c8e65b820fb60f5a4f9f71b99618
[yunchengzhang1] <yunchengz@smu.edu>
2022-04-08 15:42:14 -0500

    Update auth.js
    get rid of claims

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -21,27 +21,5 @@ const authenticateJWT = (req, res, next) => {
   });
 };
 
-const authenticateWithClaims = (claims) => (req, res, next) => {
-  const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.sendStatus(401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, accessTokenSecret, (err, user) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
-    for (let claim of claims) {
-      if (user.claims.includes(claim)) {
-        req.user = user;
-        return next();
-      }
-    }
-    return res.sendStatus(403);
-  });
-}
-
-module.exports = { authenticateJWT, authenticateWithClaims };
+module.exports = { authenticateJWT };
