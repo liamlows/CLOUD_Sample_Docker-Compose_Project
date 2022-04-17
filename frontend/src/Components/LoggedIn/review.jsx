743d6f9442d0521e3d96456dae85fccd31d3aabe
RebeccaEKuhlman <rkuhlman@smu.edu>
2022-04-16 21:33:56 -0500

    Added review and review form

--- frontend/src/Components/LoggedIn/review.jsx
+++ frontend/src/Components/LoggedIn/review.jsx
@@ -1,14 +1,25 @@
-/**
- * Course review (both the same professor and course)
- */
- export const review = ({course}) => {
+import React,{ useState, useEffect } from 'react';
+export const ReviewList = (props) =>{
+    console.log(props.reviews);
 
-    //TODO: add course review table
-    return <section id="review">
-        <h1>{course.className}</h1>
-        <p>{course.description}</p>
-        <p onclick={()=>navigate(`/users/${course.professor}`)}>{course.professor}</p> 
-        <p>{course.average}</p>
-        
-    </section>
+    if(props.reviews.length === 0){
+        return <h5>Be the first to add a review!</h5>
+    }
+
+    return <div className='container'>
+        <h4>Course Reviews <span>({props.reviews.length})</span></h4>
+        <div className="mt--4 mb--2">
+        {
+            props.reviews.map((review, index) =>
+            <div key = {index}> {review.name}
+                <div>{review.rating}</div>
+                <div>
+                    <span>{review.date}</span>
+                    <p>{review.userName}</p> 
+                    <p>"{review.comment}"</p>
+                </div>
+                
+            </div>
+            )}
+    </div></div>
 }
