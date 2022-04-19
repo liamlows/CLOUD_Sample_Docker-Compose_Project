07139e8cec75a997acce595d177e2eafb4607870
Elliot Boesch <elliot.boesch@gmail.com>
2022-04-18 19:28:24 -0500

    Adding MUI labs for Home

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,10 +1,36 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 export const Home = () => {
-    return (
-        <>
-            <h1>Farm Finders</h1>
-            
-        </>
     
-    )
+
+    return (
+        <Timeline>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot variant="outlined" color="primary"/>
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Sell</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot variant="outlined" color="primary"/>
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent>Buy</TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot variant="outlined" color="primary"/>
+                </TimelineSeparator>
+                <TimelineContent>Connect</TimelineContent>
+            </TimelineItem>
+        </Timeline>
+    );
 }
