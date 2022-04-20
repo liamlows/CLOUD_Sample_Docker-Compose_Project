9e3826e05b2730c1dcae748afde000ed716e2530
AliceWonderland2019 <alice100gm@gmail.com>
2022-04-20 18:47:10 -0500

    new upadate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { Messages } from "./messages/Messages";
 import { Search } from "./search/Search";
 import { Notifications } from "./notifications/Notifications";
 import { EditProfile } from './profile/EditProfile';
+import { NFTDetail} from'./home/NFTDetail';
 
 // React functional component
 function App () {
@@ -25,6 +26,7 @@ function App () {
       <Route exact path="/search" element={<Search/>} />
       <Route exact path="/notifications" element={<Notifications/>} />
       <Route exact path="/edit-profile" element={<EditProfile/>} />
+      <Route exact path="/NFT-details" element={<NFTDetail/>}/>
     </Routes>
 </div>
     );
