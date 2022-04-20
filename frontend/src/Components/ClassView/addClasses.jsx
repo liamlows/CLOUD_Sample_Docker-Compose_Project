66e46e4c76359902ecdb562d072af72297e57714
wesoa012 <wesa@smu.edu>
2022-04-20 15:42:46 -0500

    New Page

---
+++ frontend/src/Components/ClassView/addClasses.jsx
@@ -0,0 +1,11 @@
+import { StickyTable } from "./stickyTable";
+import { getAllCourses, getClasses } from "../../APIFolder/loginApi";
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+
+export const addClasses = () => {
+
+
+
+
+}
