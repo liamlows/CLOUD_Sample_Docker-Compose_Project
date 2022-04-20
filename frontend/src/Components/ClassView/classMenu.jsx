8dd2fb24a07276adc126bf35a9a2c5ffd40c23b2
wesoa012 <wesa@smu.edu>
2022-04-20 15:42:52 -0500

    Re doing

--- frontend/src/Components/ClassView/classMenu.jsx
+++ frontend/src/Components/ClassView/classMenu.jsx
@@ -1,10 +1,27 @@
-import {StickyTable} from "../LoggedIn/stickyTable";
-import { getAllCourses } from "../../APIFolder/loginApi";
+import { StickyTable } from "./stickyTable";
+import { getAllCourses, getClasses } from "../../APIFolder/loginApi";
+import { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import Add from "@mui/icons-material/Add";
 
 export const ClassMenu = () => {
+  const [classes, setClasses] = useState([]);
+  const navigate = useNavigate();
 
-    //TODO: put nav bar in, data intergration
-    return <div>
-        <StickyTable rows = {createData({getAllCourses})} columns = {columnsMenu}/>
-     </div>
+
+  useEffect(() => {
+    getClasses().then(res => {
+      setClasses(...res)
+    })
+  }, []);
+  const goToCourseAdd = () => {
+    navigate('/classes/enrollment');
+  }
+
+  //TODO: put nav bar in, data intergration
+  return <section className="ClassesMenu">
+    <Button variant="contained" className="bg-success" onClick={() => goToCourseAdd()} endIcon={<Add />}>Add Class</Button>
+    <StickyTable rows={classes} />
+  </section>
 };
