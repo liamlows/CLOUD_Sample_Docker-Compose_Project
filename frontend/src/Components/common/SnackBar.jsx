994686dab3651d7224975c440c1e089f2b4ebaf8
RebeccaEKuhlman <rkuhlman@smu.edu>
2022-04-20 18:40:23 -0500

    Create SnackBar.jsx

---
+++ frontend/src/Components/common/SnackBar.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function CustomizedSnackbars() {
+  const [open, setOpen] = React.useState(false);
+  /* Needs to access schedule to add course to, have alternative sitution if seats >= max seats*/
+
+  const handleClick = () => {
+    if(true)
+        setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Stack spacing={2} sx={{ width: '100%' }}>
+      <Button variant="outlined" onClick={handleClick}>
+        Add course
+      </Button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+        Course Added Sucessfully
+        </Alert>
+      </Snackbar>
+      <Alert severity="success">Course Added Sucessfully</Alert>
+    </Stack>
+  );
+}
