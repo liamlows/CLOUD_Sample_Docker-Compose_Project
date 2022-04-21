5fd64a01aca1c7309e70a878b78c56fe7956b1d0
wesoa012 <wesa@smu.edu>
2022-04-20 23:59:01 -0500

    Still working on it

--- frontend/src/Components/ClassView/addClasses.jsx
+++ frontend/src/Components/ClassView/addClasses.jsx
@@ -2,9 +2,9 @@ import { getAllCourses, getClasses } from "../../APIFolder/loginApi";
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 
-export const addClasses = () => {
+export const AddClasses = () => {
 
-
+return <div></div>
 
 
 }
