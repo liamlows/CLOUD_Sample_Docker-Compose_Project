ef12afee4276212a80d08611849a9a94365c84e8
kirkwat <60279003+kirkwat@users.noreply.github.com>
2022-04-23 18:56:20 -0500

    epic 4 create cart/order

--- backend/database/framework.sql
+++ backend/database/framework.sql
@@ -19,29 +19,35 @@ CREATE TABLE users(
     last_name VARCHAR(50),
     isFarmer BOOL default false
 );
--- EPIC 7
+-- EPIC 4
 CREATE TABLE transactions(
 	transaction_id integer not null auto_increment, primary key(transaction_id),
-    customer_id VARCHAR(50),
+    customer_id VARCHAR(50), 
     farmer_id VARCHAR(50),
     product_id VARCHAR(50),
     quantity integer,
-    total_price varchar(50) not null,
+    price varchar(50) not null,
     product_name VARCHAR(50) NOT NULL,
-    product_price VARCHAR(50) NOT NULL
+    purchaseDate VARCHAR(50),
+    
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    address VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    zip VARCHAR(50),
+    cardName VARCHAR(50),
+    cardNumber VARCHAR(50),
+    cardExprDate VARCHAR(50)
 );
-INSERT INTO transactions(customer_id,farmer_id,product_id,quantity,total_price,product_name,product_price)
-VALUES
-('kirk2@smu.edu','charlie4@smu.edu','123',8,'$30','1 Cow','$123'),
-('kirk2@smu.edu','charlie4@smu.edu','234',7,'$30','2 Pigs','$234'),
-('kirk2@smu.edu','charlie3@smu.edu','345',6,'$30','1 Sheep','345'),
-('kirk2@smu.edu','charlie3@smu.edu','456',5,'$30','3 Chickens','456'),
-('kirk1@smu.edu','charlie3@smu.edu','567',4,'$30','1 Goat','567'),
-('kirk1@smu.edu','charlie3@smu.edu','678',3,'$30','1 Cow','123'),
-('kirk1@smu.edu','charlie4@smu.edu','789',2,'$30','4 Sheep','345'),
-('kirk1@smu.edu','charlie4@smu.edu','890',1,'$30','2 Goats','567');
-select * from transactions;
-drop tables transactions;
+CREATE TABLE cart(
+    cart_id INTEGER NOT NULL AUTO_INCREMENT, PRIMARY KEY (cart_id),
+    cart_price INTEGER,
+    quantity INTEGER,
+    customer_id VARCHAR(50),
+    product_id INTEGER
+);
+-- EPIC 7
 CREATE TABLE event(
     event_id integer not null auto_increment, PRIMARY KEY(event_id),
     event_name VARCHAR(50),
@@ -56,21 +62,8 @@ CREATE TABLE customer_event_interests(
     farmer_id VARCHAR(50),
     customer_id VARCHAR(50)
 );
-drop tables event;
-drop tables customer_event_interests;
-select * from event;
-select * from customer_event_interests;
-INSERT INTO event(event_name,event_description,farmer_id)
-VALUES
-('Farm show 1','see animals','charlie3@smu.edu'),
-('Farm show 2','see dogs','charlie3@smu.edu'),
-('Farm show 3','see cats','charlie4@smu.edu');
-INSERT INTO customer_event_interests(event_id,event_name,event_description,farmer_id,customer_id)
-VALUES
-(1,'Farm show 1','see animals','charlie3@smu.edu','kirk2@smu.edu'),
-(2,'Farm show 2','see dogs','charlie3@smu.edu','kirk2@smu.edu'),
-(3,'Farm show 3','see cats','charlie4@smu.edu','kirk1@smu.edu');
 
+-- OLD OLD OLD
 /*------------------------APP CAPABILITIES----------------------------*/
 
 CREATE TABLE requests(
