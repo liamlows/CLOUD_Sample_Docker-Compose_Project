345c66c075b156a20b4cfa80c4e79ea7d991663e
kirkwat <60279003+kirkwat@users.noreply.github.com>
2022-04-24 18:30:59 -0500

    modifying for frontend + inserts

--- backend/database/framework.sql
+++ backend/database/framework.sql
@@ -6,8 +6,7 @@ USE db;
 
 
 DROP TABLE customer_event_interests, farmer, product, transactions, users, requests, customer_inventory, cart, event;
--- NEW NEW NEW NEW NEW
--- EPIC 1
+
 
 CREATE TABLE users(
     user_id INTEGER AUTO_INCREMENT, PRIMARY KEY(user_id),
@@ -17,7 +16,6 @@ CREATE TABLE users(
     last_name VARCHAR(50),
     isFarmer tinyint(1) NOT NULL DEFAULT FALSE
 );
-
 CREATE TABLE farmer (
     farmer_id INTEGER AUTO_INCREMENT, PRIMARY KEY (farmer_id),
     farm_name VARCHAR(50),
@@ -26,9 +24,6 @@ CREATE TABLE farmer (
     date_founded DATE,
     owner_id INTEGER NOT NULL, FOREIGN KEY (owner_id) REFERENCES users(user_id)
 );
--- EPIC 4
-
-SELECT * FROM users;
 CREATE TABLE transactions(
 	transaction_id integer auto_increment, primary key(transaction_id),
     customer_id INTEGER NOT NULL, FOREIGN KEY (customer_id) REFERENCES users(user_id),
@@ -62,24 +57,16 @@ CREATE TABLE event(
     event_id integer auto_increment, PRIMARY KEY(event_id),
     event_name VARCHAR(50),
     event_description VARCHAR(50),
-    farmer_id INTEGER NOT NULL, FOREIGN KEY (farmer_id) REFERENCES farmer(farmer_id)
+    farmer_id INTEGER NOT NULL, FOREIGN KEY (farmer_id) REFERENCES farmer(farmer_id),
+    date VARCHAR(50),
+    time VARCHAR(50)
 );
 CREATE TABLE customer_event_interests(
 	customer_event_interests_id integer auto_increment, PRIMARY KEY(customer_event_interests_id),
     event_id VARCHAR(50),
-    event_name VARCHAR(50),
-    event_description VARCHAR(50),
-    farmer_id INTEGER NOT NULL, FOREIGN KEY (farmer_id) REFERENCES farmer(farmer_id),
     customer_id INTEGER NOT NULL, FOREIGN KEY (customer_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE cart(
-    cart_id INTEGER NOT NULL AUTO_INCREMENT, PRIMARY KEY (cart_id),
-    product_count INTEGER,
-    user_id INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES users(user_id),
-    cart_price INTEGER,
-    quantity INTEGER
-);
 CREATE TABLE product(
     product_id INTEGER NOT NULL AUTO_INCREMENT,  PRIMARY KEY(product_id),
     product_name VARCHAR(50) NOT NULL,
@@ -117,3 +104,19 @@ VALUES ('smu@email.edu', 'Password123', 'John', 'Deere', 1),
 INSERT INTO farmer(farm_name, farm_description, farm_image_url, date_founded, owner_id)
 VALUES ('Johns Farm', 'LETS GOOOOO', 'https://st.depositphotos.com/1333205/2857/i/600/depositphotos_28571959-stock-photo-farm-building.jpg', '1999-03-23', 1);
 
+-- event transaction cart
+INSERT INTO event(event_name,event_description,farmer_id,date,time)
+VALUES
+('Animal Show','All Animals 50% off','smu@email.edu''4/24/22','5:00 PM'),
+('Plant Show','Buy one get one 50% off','smu@email.edu''5/1/22','10:00 AM'),
+('Dog Show','Tricks and treats','smu@email.edu','6/1/22','11:00 AM');
+
+INSERT INTO customer_event_interests(event_id,customer_id)
+VALUES
+(1,'cool@gmail.com'),
+(3,'cool@gmail.com');
+
+INSERT INTO transactions(customer_id,farmer_id,product_id,quantity,is_complete,total_price,purchaseDate, firstName,lastName,address,city,state,zip,cardName,cardNumber,cardExprDate)
+VALUES
+('cool@gmail.com','smu@email.edu',1,2,1,'$100','4/24/22','Mark','Fontenot','123 elm stree','dallas','tx','75205','visa','1111222233334444','05/22'),
+('cool@gmail.com','smu@email.edu',2,1,0,'$50','5/1/22','Mark','Fontenot','123 elm stree','dallas','tx','75205','visa','1111222233334444','05/22');
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res, next) => {
     try {
         const body = req.body;
         console.log(body);
-        const result = await User.createNewUser(body.email, body.password, body.isFarmer);
+        const result = await User.createNewUser(body.first_name, body.last_name, body.email, body.password, body.isFarmer);
         res.status(201).json(result);
     } catch (err) {
         console.error('Failed to create new user:', err);
