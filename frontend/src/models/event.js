781e94713fce25a2a3270802543061dda70a73be
mharrison777 <marycateharrison@gmail.com>
2022-04-24 19:17:36 -0500

    event page styling

--- frontend/src/models/event.js
+++ frontend/src/models/event.js
@@ -1,6 +1,8 @@
 export class event{
-    constructor (eventTitle, eventDescription, eventId, farmName, farmId, userId, eventImage) {
+    constructor (eventTitle, eventDescription, eventId, eventTime, eventDate, farmName, farmId, userId, eventImage) {
         this.eventTitle = eventTitle;
+        this.eventTime = eventTime;
+        this.eventDate = eventDate;
         this.eventDescription = eventDescription;
         this.eventId = eventId;
         this.eventImage = eventImage;
