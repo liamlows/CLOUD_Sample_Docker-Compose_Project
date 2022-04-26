48aca6cf2096bba7b5d167e6369ffec1f2c5ed1a
kirkwat <60279003+kirkwat@users.noreply.github.com>
2022-04-25 21:27:09 -0500

    login update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,6 +7,9 @@ router.post('/', async (req, res, next) => {
         const body = req.body;
         console.log(body);
         const result = await User.createNewUser(body.first_name, body.last_name, body.email, body.password, body.isFarmer);
+        if(result.length === 0){
+            res.status(500).json({ message: 'Invalid login information' });
+        }
         res.status(201).json(result);
     } catch (err) {
         console.error('Failed to create new user:', err);
