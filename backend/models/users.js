7c247b693395c53542b0e9e36a09cc4c3337afe9
pmcfarlain <78279515+pmcfarlain@users.noreply.github.com>
2022-04-27 10:58:31 -0500

    User cannot make a new account with an existing username or email

--- backend/models/users.js
+++ backend/models/users.js
@@ -16,10 +16,20 @@ const createNewUser = async (username, name, email, password, privileges, photo)
     const hashedPassword = await bcrypt.hash(password, salt);
     console.log('Hashed password', hashedPassword);
 
-    // Inserts into user table
-    const query = knex(USER_TABLE).insert({ username, name, email, password: hashedPassword, privileges, photo });
-    const result = await query;
-    return result;
+    const exists = await findUserByEmail(email);
+    const exists1 = await findUserByUsername(username);
+
+    if(exists.length === 0 && exists1.length === 0){
+        // Inserts into user table
+        const query = knex(USER_TABLE).insert({ username, name, email, password: hashedPassword, privileges, photo });
+        const result = await query;
+        return result;
+    } else {
+        console.log("Username or email already exists.");
+        return res.sendStatus(401);
+    }
+
+   
 };
 
 // Locates a user record by email
