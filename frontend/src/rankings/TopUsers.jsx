e2fb8603112d81a22d89e532435ca841f92e37f4
katebouis <78283299+katebouis@users.noreply.github.com>
2022-04-27 13:18:25 -0500

    fixed rankings 404

--- frontend/src/rankings/TopUsers.jsx
+++ frontend/src/rankings/TopUsers.jsx
@@ -2,54 +2,50 @@ import './rankings.css';
 import * as React from 'react';
 import { Button, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { getUsers } from '../api/UsersAPI';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { useState, useEffect } from "react";
 
 export const TopUsers = () => {
+    const[ users, setUsers ] = useState(undefined);
+
+    useEffect(() => {
+        getUsers().then(x => setUsers(x));
+    }, [ ]);
+
+    if(!users){
+        return<><Box sx={{ mx:"auto"}}>
+        <CircularProgress color="secondary" />
+        </Box></>
+    }
+    var place = 1;
 
     return(<div className="rankingContainer">
+
+
         <h5 className="rankingTitle">Users</h5>
         <Stack spacing={3}>
             <div class="row"/>
-            <div class="row">
-                <div class="col-4">
-                    <Link to='/theirUser' className='createLink'>1. User</Link>
-                </div>
-                <div class="col-4">
-                    Value
-                </div>
-                <div class="col-4">
-                <Button color="secondary" onClick={() => {alert('User followed');}} variant="contained">
-                        Follow
-                    </Button>
-                </div>
-            </div>
 
-            <div class="row">
-                <div class="col-4">
-                    <Link to='/theirUser' className='createLink'>2. User</Link>
-                </div>
-                <div class="col-4">
-                    Value
-                </div>
-                <div class="col-4">
-                <Button color="secondary" onClick={() => {alert('User followed');}} variant="contained">
+            {
+                users.map(user => <div key={user.id} class="row">
+                    <div class="col-4">
+                      {place++}. 
+                      <Link to={`/user/${user.id}`} className="createLink">
+                        {user.username}
+                      </Link>
+                    </div>
+                    <div class="col-4">
+                        ${user.balance}
+                    </div>
+                    <div class="col-4">
+                    <Button color="secondary" onClick={() => {alert('User followed');}} variant="contained">
                         Follow
                     </Button>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-4">
-                    <Link to='/theirUser' className='createLink'>3. User</Link>
-                </div>
-                <div class="col-4">
-                    Value
-                </div>
-                <div class="col-4">
-                <Button color="secondary" onClick={() => {alert('User followed');}} variant="contained">
-                        Follow
-                    </Button>
-                </div>
-            </div>
+                    </div>
+                    </div>)
+            }
         </Stack>
     </div>);
 }
